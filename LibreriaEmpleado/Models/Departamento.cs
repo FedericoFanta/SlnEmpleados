66ba93010517bdfdcddf943c623262124dba5bfd
Federico Fantacone <fede26f@hotmail.com>
2022-11-11 18:02:14 -0300

    Trabajo terminado

--- LibreriaEmpleado/Models/Departamento.cs
+++ LibreriaEmpleado/Models/Departamento.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibreriaEmpleado.Models
 {
+    [Table("Departamento")]
     public class Departamento
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
         public string Nombre { get; set; }
         public int Empleados { get; set; }
 
--- LibreriaEmpleado/Models/Empleado.cs
+++ LibreriaEmpleado/Models/Empleado.cs
@@ -1,16 +1,18 @@
 ﻿using LibreriaEmpleado.Models.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibreriaEmpleado.Models
 {
+    [Table("Empleado")]
     public class Empleado : Persona
     {
-       
-
+        
         public Empleado(Departamento departamento,int id, string nombre, string apellido, string legajo) : base(id, nombre, apellido)
         {
             Legajo=legajo;  
