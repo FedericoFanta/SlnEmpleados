dda3da844ecfdc6ba5ca81dbee162a35b4aa8535
FedericoFanta <110657439+FedericoFanta@users.noreply.github.com>
2022-11-15 14:12:08 -0300

    Update DbEmpleadosContext.cs

--- WindowsEmpleados/Data/DbEmpleadosContext.cs
+++ WindowsEmpleados/Data/DbEmpleadosContext.cs
@@ -17,5 +17,20 @@ namespace WindowsEmpleados.Data
         public DbSet<Departamento> Departamentos { get; set; }
 
         public DbSet<Empleado> Empleados { get; set; }
+        
+         protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Empleado>().HasKey(e => e.Id);
+
+            modelBuilder.Entity<Empleado>().Property(e=>e.Legajo).HasColumnType("varchar").HasMaxLength(10).IsRequired();
+            modelBuilder.Entity<Empleado>().Property(e => e.Nombre).HasColumnType("varchar").HasMaxLength(40).IsRequired();
+            modelBuilder.Entity<Empleado>().Property(e => e.Apellido).HasColumnType("varchar").HasMaxLength(20).IsRequired();
+
+            modelBuilder.Entity<Departamento>().HasKey(e => e.Id);
+
+            modelBuilder.Entity<Departamento>().Property(e => e.Nombre).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+        }
     }
 }
